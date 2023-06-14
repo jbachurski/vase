3b0394f362058e2bcd4d5456ffb166b2e85c7420
jbachurski <kbachurski@gmail.com>
2023-06-15 00:48:10 +0100

    Add proper tests

--- test/test_regex.ml
+++ test/test_regex.ml
@@ -1,11 +1,25 @@
 open Vase
 
-let test_ab () =
-  Alcotest.(check bool)
-    "matches" true
-    (Regex.matches []
-       (Regex.concat (Regex.star (Regex.symbol 'a')) (Regex.star (Regex.symbol 'b'))))
+let test_matches (msg, is, what, where) =
+  Alcotest.test_case msg `Quick (fun () ->
+      Alcotest.(check bool) "matches" is (Regex.matches what where))
+
+let a_b_star = Regex.concat (Regex.symbol 'a') (Regex.star (Regex.symbol 'b'))
 
 let () =
   let open Alcotest in
-  run "Regex" [ ("matches", [ test_case "a*b*" `Quick test_ab ]) ]
+  run "Regex"
+    [
+      ( "matches",
+        List.map test_matches
+          [
+            ("| </= 0", false, [], Regex.nothing);
+            ("| <= |", true, [], Regex.null);
+            ("a <= a", true, [ 'a' ], Regex.symbol 'a');
+            ("b </= a", false, [ 'b' ], Regex.symbol 'a');
+            ("| <= ab*", false, [], a_b_star);
+            ("a <= ab*", true, [], a_b_star);
+            ("abbb <= ab*", true, [], a_b_star);
+            ("aabbb </= ab*", false, [], a_b_star);
+          ] );
+    ]
