2e412717e5a51ad76a9c74297043a299e8f6a9a6
jbachurski <kbachurski@gmail.com>
2023-06-15 01:08:06 +0100

    Add more tests

--- test/test_regex.ml
+++ test/test_regex.ml
@@ -4,7 +4,15 @@ let test_matches (msg, is, what, where) =
   Alcotest.test_case msg `Quick (fun () ->
       Alcotest.(check bool) "matches" is (Regex.matches what where))
 
-let a_b_star = Regex.concat (Regex.symbol 'a') (Regex.star (Regex.symbol 'b'))
+let ( ~^ ) = Regex.symbol
+let ( ~* ) = Regex.star
+let ( ~! ) = Regex.complement
+let ( +>>+ ) = Regex.concat
+let ( +|+ ) = Regex.union
+let ( +&+ ) = Regex.intersect
+let a_b_star = ~^'a' +>>+ ~*(~^'b')
+let ab_or_ac = ~^'a' +>>+ ~^'b' +|+ (~^'a' +>>+ ~^'c')
+let a_and_a_star = ~^'a' +&+ ~*(~^'a')
 
 let () =
   let open Alcotest in
@@ -15,11 +23,18 @@ let () =
           [
             ("| </= 0", false, [], Regex.nothing);
             ("| <= |", true, [], Regex.null);
-            ("a <= a", true, [ 'a' ], Regex.symbol 'a');
-            ("b </= a", false, [ 'b' ], Regex.symbol 'a');
+            ("a <= a", true, [ 'a' ], ~^'a');
+            ("b </= a", false, [ 'b' ], ~^'a');
             ("| <= ab*", false, [], a_b_star);
             ("a <= ab*", true, [ 'a' ], a_b_star);
             ("abbb <= ab*", true, [ 'a'; 'b'; 'b'; 'b' ], a_b_star);
             ("aabbb </= ab*", false, [ 'a'; 'a'; 'b'; 'b' ], a_b_star);
+            ("ab <= ab+ac", true, [ 'a'; 'b' ], ab_or_ac);
+            ("ac <= ab+ac", true, [ 'a'; 'c' ], ab_or_ac);
+            ("a </= ab+ac", false, [ 'a' ], ab_or_ac);
+            ("a <= a & a*", true, [ 'a' ], a_and_a_star);
+            ("| </= a & a*", false, [], a_and_a_star);
+            ("aa </= a & a*", false, [ 'a'; 'a' ], a_and_a_star);
+            ("aabbb </= !(ab*)", true, [ 'a'; 'a'; 'b'; 'b' ], ~!a_b_star);
           ] );
     ]
