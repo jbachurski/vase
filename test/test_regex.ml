8220f29c8e537aa46ef8d6f276387fe90c6d655c
jbachurski <kbachurski@gmail.com>
2023-06-15 11:39:56 +0100

    Play with more tests

--- test/test_regex.ml
+++ test/test_regex.ml
@@ -2,7 +2,16 @@ open Vase
 
 let test_matches (msg, is, what, where) =
   Alcotest.test_case msg `Quick (fun () ->
-      Alcotest.(check bool) "matches" is (Regex.matches what where))
+      Alcotest.(check bool) "matches" is (Regex.matches what where);
+      if is then
+        Alcotest.(check bool)
+          "star-matches-2" true
+          (Regex.matches (what @ what) (Regex.star where))
+      else ();
+      Alcotest.(check bool)
+        "complement-matches" (not is)
+        (Regex.matches what (Regex.complement where));
+      ())
 
 let ( ~^ ) = Regex.symbol
 let ( ~* ) = Regex.star
@@ -14,27 +23,25 @@ let a_b_star = ~^'a' +>>+ ~*(~^'b')
 let ab_or_ac = ~^'a' +>>+ ~^'b' +|+ (~^'a' +>>+ ~^'c')
 let a_and_a_star = ~^'a' +&+ ~*(~^'a')
 
+let regex_cases =
+  [
+    ("| </= 0", false, [], Regex.nothing);
+    ("| <= |", true, [], Regex.null);
+    ("a <= a", true, [ 'a' ], ~^'a');
+    ("b </= a", false, [ 'b' ], ~^'a');
+    ("| <= ab*", false, [], a_b_star);
+    ("a <= ab*", true, [ 'a' ], a_b_star);
+    ("abbb <= ab*", true, [ 'a'; 'b'; 'b'; 'b' ], a_b_star);
+    ("aabbb </= ab*", false, [ 'a'; 'a'; 'b'; 'b' ], a_b_star);
+    ("ab <= ab+ac", true, [ 'a'; 'b' ], ab_or_ac);
+    ("ac <= ab+ac", true, [ 'a'; 'c' ], ab_or_ac);
+    ("a </= ab+ac", false, [ 'a' ], ab_or_ac);
+    ("a <= a & a*", true, [ 'a' ], a_and_a_star);
+    ("| </= a & a*", false, [], a_and_a_star);
+    ("aa </= a & a*", false, [ 'a'; 'a' ], a_and_a_star);
+    ("aabbb </= !(ab*)", true, [ 'a'; 'a'; 'b'; 'b' ], ~!a_b_star);
+  ]
+
 let () =
   let open Alcotest in
-  run "Regex"
-    [
-      ( "matches",
-        List.map test_matches
-          [
-            ("| </= 0", false, [], Regex.nothing);
-            ("| <= |", true, [], Regex.null);
-            ("a <= a", true, [ 'a' ], ~^'a');
-            ("b </= a", false, [ 'b' ], ~^'a');
-            ("| <= ab*", false, [], a_b_star);
-            ("a <= ab*", true, [ 'a' ], a_b_star);
-            ("abbb <= ab*", true, [ 'a'; 'b'; 'b'; 'b' ], a_b_star);
-            ("aabbb </= ab*", false, [ 'a'; 'a'; 'b'; 'b' ], a_b_star);
-            ("ab <= ab+ac", true, [ 'a'; 'b' ], ab_or_ac);
-            ("ac <= ab+ac", true, [ 'a'; 'c' ], ab_or_ac);
-            ("a </= ab+ac", false, [ 'a' ], ab_or_ac);
-            ("a <= a & a*", true, [ 'a' ], a_and_a_star);
-            ("| </= a & a*", false, [], a_and_a_star);
-            ("aa </= a & a*", false, [ 'a'; 'a' ], a_and_a_star);
-            ("aabbb </= !(ab*)", true, [ 'a'; 'a'; 'b'; 'b' ], ~!a_b_star);
-          ] );
-    ]
+  run "Regex" [ ("matches", List.map test_matches regex_cases) ]
