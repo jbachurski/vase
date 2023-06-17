a7d4f05e50d531f6fdd7ef76211b9d0e21d90bfc
jbachurski <kbachurski@gmail.com>
2023-06-17 01:17:51 +0100

    Rename test file

--- test/test_vase.ml
+++ test/test_vase.ml
@@ -1,4 +1,4 @@
 let () =
   let open Alcotest in
   run "Vase"
-    (List.concat [ Test_regex.test_cases; Test_lexer.test_cases; Test_parc.test_cases ])
+    (List.concat [ Test_regex.test_cases; Test_lexer.test_cases; Test_parser.test_cases ])
